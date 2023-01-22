9548872b39986d2a302145a5618cb711bdb49377
Gabriel Porto <7gabrielporto@gmail.com>
2023-01-22 02:37:19 -0300

    feat: add CurrentDay on interface

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -17,6 +17,8 @@ export function HabitDay({ defaultCompleted = 0, amount = 0, date }: HabitDayPro
 
     const completedPercentage = amount > 0 ? Math.round((completed / amount) * 100) : 0
 
+    const today = dayjs().startOf('day').toDate();
+    const isCurrentDay = dayjs(date).isSame(today);
     const dayAndMonth = dayjs(date).format('DD/MM')
     const dayOfWeek = dayjs(date).format('dddd')
 
@@ -34,6 +36,7 @@ export function HabitDay({ defaultCompleted = 0, amount = 0, date }: HabitDayPro
                     'bg-violet-700 border-violet-500': completedPercentage >= 40 && completedPercentage < 60,
                     'bg-violet-600 border-violet-500': completedPercentage >= 60 && completedPercentage <= 80,
                     'bg-violet-500 border-violet-400': completedPercentage > 80,
+                    ["border-white border-4"] : isCurrentDay
                 })}
             />
 
